386d70ffe7f0ad4f7fc5f9c7e0d8aa4e59cfb34c
Vojislav <vojislav.stupar@gmail.com>
2019-05-28 20:01:42 +0200

    zadatak3

---
+++ resources/views/movies/show.blade.php
@@ -0,0 +1,11 @@
+
+
+
+
+   <h3>{{$movie->title}}</h3>
+   <p>{{$movie->genre}}</p>
+   <p>{{$movie->director}}</p>
+   <p>{{$movie->year}}</p>
+   <p>{{$movie->storyline}}</p>
+
+
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/movies' , 'MoviesController@index'); 
+
+Route::get('/movies/{id}' , 'MoviesController@show');
